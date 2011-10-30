4f8b9857635e2cc1109fea6274f3dafacaf2626c
Derek Mansen <dmansen@rjmetrics.com>
2011-10-30 09:32:14 -0400

    status check

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
                  [compojure "0.6.4"]
                  [hiccup "0.3.6"]
                  [rhino/js "1.7R2"]
-                 [sandbar/sandbar "0.2.3"]]
+                 [sandbar/sandbar "0.2.3"]
+                 [org.xerial/sqlite-jdbc "3.6.16"]]
   :dev-dependencies [[lein-ring "0.4.5"]]
   :ring {:handler blog.routes/app})
--- src/blog/sql.clj
+++ src/blog/sql.clj
@@ -1,1 +1,24 @@
-(ns blog.sql)
+(ns blog.sql
+  (:require [clojure.contrib.sql :as sql]))
+
+; Give the sql library all the information about the db
+; to be used in transactions
+(def db {:classname "org.sqlite.JDBC"
+         :subprotocol "sqlite"         ; Protocol to use
+         :subname "db/db.sqlite3"      ; Location of db
+         :create true})
+
+(. Class (forName "org.sqlite.JDBC")) ; Initialize the JDBC driver
+
+(defn db-create
+  "Creates the table for this model"
+  []
+  (sql/create-table
+   :something
+   [:id :int "PRIMARY KEY"]
+   [:name "varchar(32)"]))
+
+(sql/with-connection
+  db
+  (sql/transaction
+   (db-create)))
